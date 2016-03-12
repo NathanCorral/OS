149f314b584b6e5148ebfc01b2f71f6192ba2cf3
mflavoi2 <mflavoi2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-12 21:48:46 +0000

    separated interrupt init

---
+++ student-distrib/interrupts.c
@@ -0,0 +1,82 @@
+
+#include "x86_desc.h"
+#include "lib.h"
+#include "i8259.h"
+#include "debug.h"
+
+#include "errors.h"
+#include "keyboard.h"
+#include "keyboardirq.h"
+#include "rtc.h"
+#include "rtcirq.h"
+
+void interruptinit(){
+	int i;
+for(i=0; i<0x20; i++){
+	
+	idt[i].present = 1;
+    idt[i].dpl = 0;
+    idt[i].reserved0 = 0;
+    idt[i].size = 1;
+    idt[i].reserved1 = 1;
+    idt[i].reserved2 = 1;
+    idt[i].reserved3 = 1;
+    idt[i].reserved4 = 0;
+    idt[i].seg_selector = KERNEL_CS;
+}
+
+//interrupts
+for(i=0x20; i<0x2F; i++){
+	idt[i].present = 1;
+    idt[i].dpl = 0;
+    idt[i].reserved0 = 0;
+    idt[i].size = 1;
+    idt[i].reserved1 = 1;
+    idt[i].reserved2 = 1;
+    idt[i].reserved3 = 0;
+    idt[i].reserved4 = 0;
+    idt[i].seg_selector = KERNEL_CS;
+}
+
+
+	SET_IDT_ENTRY(idt[0], dividebyzero);
+	SET_IDT_ENTRY(idt[1], debugger);
+	SET_IDT_ENTRY(idt[2], nmi);
+	SET_IDT_ENTRY(idt[3], breakpoint);
+	SET_IDT_ENTRY(idt[4], overflow);
+	SET_IDT_ENTRY(idt[5], bounds);
+	SET_IDT_ENTRY(idt[6], invalidopcode);
+	SET_IDT_ENTRY(idt[7], coprocessornotavailable);
+	SET_IDT_ENTRY(idt[8], doublefault);
+	SET_IDT_ENTRY(idt[9], coprocessorsegoverrun);
+	SET_IDT_ENTRY(idt[10], invalidtask);
+	SET_IDT_ENTRY(idt[11], segnotpresent);
+	SET_IDT_ENTRY(idt[12], stackfault);
+	SET_IDT_ENTRY(idt[13], genprotection);
+	SET_IDT_ENTRY(idt[14], pagefault);
+	SET_IDT_ENTRY(idt[15], reserved);
+	SET_IDT_ENTRY(idt[16], mathfault);
+	SET_IDT_ENTRY(idt[17], aligncheck);
+	SET_IDT_ENTRY(idt[18], machinecheck);
+	SET_IDT_ENTRY(idt[19], simdfloat);
+
+	//reserved
+	for(i=20; i <= 31; i++) {
+        SET_IDT_ENTRY(idt[i], reserved);		
+    }
+	i=0x80;
+    idt[i].present = 1;
+    idt[i].dpl = 3;
+    idt[i].reserved0 = 0;
+    idt[i].size = 1;
+    idt[i].reserved1 = 1;
+    idt[i].reserved2 = 1;
+    idt[i].reserved3 = 1;
+    idt[i].reserved4 = 0;
+    idt[i].seg_selector = KERNEL_CS; 
+	SET_IDT_ENTRY(idt[0x80], syscall);
+
+SET_IDT_ENTRY(idt[33],keyboard_wrapper);
+	SET_IDT_ENTRY(idt[40],rtc_wrapper);
+
+	}
--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -14,6 +14,8 @@
 #include "rtc.h"
 #include "rtcirq.h"
  #include "paging.h"
+ #include "interrupts.h"
+
 /* Macros. */
 /* Check if the bit BIT in FLAGS is set. */
 #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
@@ -157,73 +159,73 @@ int i;
 	}
 
 	//exceptions
-for(i=0; i<0x20; i++){
+// for(i=0; i<0x20; i++){
 	
-	idt[i].present = 1;
-    idt[i].dpl = 0;
-    idt[i].reserved0 = 0;
-    idt[i].size = 1;
-    idt[i].reserved1 = 1;
-    idt[i].reserved2 = 1;
-    idt[i].reserved3 = 1;
-    idt[i].reserved4 = 0;
-    idt[i].seg_selector = KERNEL_CS;
-}
+// 	idt[i].present = 1;
+//     idt[i].dpl = 0;
+//     idt[i].reserved0 = 0;
+//     idt[i].size = 1;
+//     idt[i].reserved1 = 1;
+//     idt[i].reserved2 = 1;
+//     idt[i].reserved3 = 1;
+//     idt[i].reserved4 = 0;
+//     idt[i].seg_selector = KERNEL_CS;
+// }
 
-//interrupts
-for(i=0x20; i<0x2F; i++){
-	idt[i].present = 1;
-    idt[i].dpl = 0;
-    idt[i].reserved0 = 0;
-    idt[i].size = 1;
-    idt[i].reserved1 = 1;
-    idt[i].reserved2 = 1;
-    idt[i].reserved3 = 0;
-    idt[i].reserved4 = 0;
-    idt[i].seg_selector = KERNEL_CS;
-}
+// //interrupts
+// for(i=0x20; i<0x2F; i++){
+// 	idt[i].present = 1;
+//     idt[i].dpl = 0;
+//     idt[i].reserved0 = 0;
+//     idt[i].size = 1;
+//     idt[i].reserved1 = 1;
+//     idt[i].reserved2 = 1;
+//     idt[i].reserved3 = 0;
+//     idt[i].reserved4 = 0;
+//     idt[i].seg_selector = KERNEL_CS;
+// }
 
 
-	SET_IDT_ENTRY(idt[0], dividebyzero);
-	SET_IDT_ENTRY(idt[1], debugger);
-	SET_IDT_ENTRY(idt[2], nmi);
-	SET_IDT_ENTRY(idt[3], breakpoint);
-	SET_IDT_ENTRY(idt[4], overflow);
-	SET_IDT_ENTRY(idt[5], bounds);
-	SET_IDT_ENTRY(idt[6], invalidopcode);
-	SET_IDT_ENTRY(idt[7], coprocessornotavailable);
-	SET_IDT_ENTRY(idt[8], doublefault);
-	SET_IDT_ENTRY(idt[9], coprocessorsegoverrun);
-	SET_IDT_ENTRY(idt[10], invalidtask);
-	SET_IDT_ENTRY(idt[11], segnotpresent);
-	SET_IDT_ENTRY(idt[12], stackfault);
-	SET_IDT_ENTRY(idt[13], genprotection);
-	SET_IDT_ENTRY(idt[14], pagefault);
-	SET_IDT_ENTRY(idt[15], reserved);
-	SET_IDT_ENTRY(idt[16], mathfault);
-	SET_IDT_ENTRY(idt[17], aligncheck);
-	SET_IDT_ENTRY(idt[18], machinecheck);
-	SET_IDT_ENTRY(idt[19], simdfloat);
+// 	SET_IDT_ENTRY(idt[0], dividebyzero);
+// 	SET_IDT_ENTRY(idt[1], debugger);
+// 	SET_IDT_ENTRY(idt[2], nmi);
+// 	SET_IDT_ENTRY(idt[3], breakpoint);
+// 	SET_IDT_ENTRY(idt[4], overflow);
+// 	SET_IDT_ENTRY(idt[5], bounds);
+// 	SET_IDT_ENTRY(idt[6], invalidopcode);
+// 	SET_IDT_ENTRY(idt[7], coprocessornotavailable);
+// 	SET_IDT_ENTRY(idt[8], doublefault);
+// 	SET_IDT_ENTRY(idt[9], coprocessorsegoverrun);
+// 	SET_IDT_ENTRY(idt[10], invalidtask);
+// 	SET_IDT_ENTRY(idt[11], segnotpresent);
+// 	SET_IDT_ENTRY(idt[12], stackfault);
+// 	SET_IDT_ENTRY(idt[13], genprotection);
+// 	SET_IDT_ENTRY(idt[14], pagefault);
+// 	SET_IDT_ENTRY(idt[15], reserved);
+// 	SET_IDT_ENTRY(idt[16], mathfault);
+// 	SET_IDT_ENTRY(idt[17], aligncheck);
+// 	SET_IDT_ENTRY(idt[18], machinecheck);
+// 	SET_IDT_ENTRY(idt[19], simdfloat);
 
-	//reserved
-	for(i=20; i <= 31; i++) {
-        SET_IDT_ENTRY(idt[i], reserved);		
-    }
-	i=0x80;
-    idt[i].present = 1;
-    idt[i].dpl = 3;
-    idt[i].reserved0 = 0;
-    idt[i].size = 1;
-    idt[i].reserved1 = 1;
-    idt[i].reserved2 = 1;
-    idt[i].reserved3 = 1;
-    idt[i].reserved4 = 0;
-    idt[i].seg_selector = KERNEL_CS; 
-	SET_IDT_ENTRY(idt[0x80], syscall);
+// 	//reserved
+// 	for(i=20; i <= 31; i++) {
+//         SET_IDT_ENTRY(idt[i], reserved);		
+//     }
+// 	i=0x80;
+//     idt[i].present = 1;
+//     idt[i].dpl = 3;
+//     idt[i].reserved0 = 0;
+//     idt[i].size = 1;
+//     idt[i].reserved1 = 1;
+//     idt[i].reserved2 = 1;
+//     idt[i].reserved3 = 1;
+//     idt[i].reserved4 = 0;
+//     idt[i].seg_selector = KERNEL_CS; 
+// 	SET_IDT_ENTRY(idt[0x80], syscall);
 
-SET_IDT_ENTRY(idt[33],keyboard_wrapper);
-	SET_IDT_ENTRY(idt[40],rtc_wrapper);
-
+// SET_IDT_ENTRY(idt[33],keyboard_wrapper);
+// 	SET_IDT_ENTRY(idt[40],rtc_wrapper);
+interruptinit();
 
 //	x=5/0;
 	// i = 33;
